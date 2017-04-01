32379f72f6fdb0cf3e00968c747928f9bb4495a7
Akshay Sharma <akshay.sharma09695@gmail.com>
2017-04-01 23:30:35 +0530

    changes

--- sih17/settings.py
+++ sih17/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 MEDIA_URL = 'media/'
 
 # MODELS
