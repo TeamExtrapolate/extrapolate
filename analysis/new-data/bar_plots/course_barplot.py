20023277054c24f4b9e8d38472cd2a283a68b1ed
raunaqjain <jraunaq18@gmail.com>
2018-05-11 18:34:45 +0530

    master, added course_barplot

---
+++ analysis/new-data/bar_plots/course_barplot.py
@@ -0,0 +1,33 @@
+from utils import data_generator
+
+import pandas as pd
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import plotly.tools as tls
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
+
+plotly.tools.set_credentials_file(username='raunaq', api_key='y9L6p7Gkijuq8ptyjcS6')
+
+data = data_generator(['COURSE'])
+course_groupby = data.groupby(by='COURSE')['target'].mean()
+
+course_groupby.sort_values(ascending=False, inplace=True)
+course_groupby = course_groupby.head(20)
+
+trace = go.Bar(
+    x = course_groupby.index,
+    y = course_groupby,
+    marker = dict(
+    color = 'orange'
+    )
+)
+data = [trace]
+layout = go.Layout(
+    title = "Average Target values of 20 different courses in descending order",
+    yaxis = dict(
+        title = "Target"
+    )
+)
+fig = go.Figure(data=data, layout= layout)
+print (tls.get_embed(py.plot(fig)))
