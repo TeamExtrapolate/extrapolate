e019b9eb69a483b7cc33f8c89eae600f4eb4eb0a
raunaqjain <jraunaq18@gmail.com>
2018-05-11 18:23:43 +0530

    master, updated regional barplot

--- analysis/new-data/bar_plots/regional_barplot.py
+++ analysis/new-data/bar_plots/regional_barplot.py
@@ -4,7 +4,11 @@ import pandas as pd
 import plotly
 import plotly.plotly as py
 import plotly.graph_objs as go
+import plotly.tools as tls
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
+
+plotly.tools.set_credentials_file(username='raunaq', api_key='y9L6p7Gkijuq8ptyjcS6')
+
 
 data = data_generator(['INSTI_REGION'])
 region_group = data.groupby(by='INSTI_REGION')['target'].mean()
@@ -23,4 +27,4 @@ layout = go.Layout(
     )
 )
 fig = go.Figure(data=data, layout= layout)
-plot(fig, show_link=False)
+print (tls.get_embed(py.plot(fig)))
