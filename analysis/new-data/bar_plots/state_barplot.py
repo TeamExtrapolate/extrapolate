93a81d41c75d21ab3ddf8d144301f62463d0a930
raunaqjain <jraunaq18@gmail.com>
2018-05-12 00:02:04 +0530

    master, updated state_barplot for dash automation

--- analysis/new-data/bar_plots/state_barplot.py
+++ analysis/new-data/bar_plots/state_barplot.py
@@ -7,24 +7,48 @@ import plotly.graph_objs as go
 import plotly.tools as tls
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot,iplot
 
+from itertools import groupby
+from operator import itemgetter
+
 plotly.tools.set_credentials_file(username='raunaq', api_key='y9L6p7Gkijuq8ptyjcS6')
 
-data = data_generator(['INSTI_STATE'])
-state_group = data.groupby(by='INSTI_STATE')['target'].mean()
+# data = data_generator(['INSTI_STATE'])
+# state_group = data.groupby(by='INSTI_STATE')['target'].mean()
 
-trace = go.Bar(
-    x = state_group.index,
-    y = state_group
-)
-data = [trace]
-layout = go.Layout(
-    title = "Average Target values in different states",
-    yaxis = dict(
-        title = "Target"
-    ),
-    xaxis = dict(
-        title = "States"
+def plot(data):
+
+    grouper = itemgetter("COURSE")
+    result = []
+    for key, grp in groupby(sorted(data, key=grouper), grouper):
+        temp_dict = dict(zip(["COURSE"], [key]))
+        temp_list = [item["target"] for item in grp]
+        temp_dict['target'] = sum(temp_list) / len(temp_list)
+        result.append(temp_dict)
+
+    data = sorted(result, key = itemgetter("target"), reverse=True)
+    data = data[0:20]
+
+    x = []
+    y = []
+    for item in data:
+        x = x + [item['COURSE']]
+        y = y + [item['target']]
+
+
+    trace = go.Bar(
+        x = x,
+        y = y
     )
-)
-fig = go.Figure(data=data, layout= layout)
-print (tls.get_embed(py.plot(fig)))
+    data = [trace]
+    layout = go.Layout(
+        title = "Average Target values in different states",
+        yaxis = dict(
+            title = "Target"
+        ),
+        xaxis = dict(
+            title = "States"
+        )
+    )
+    fig = go.Figure(data=data, layout= layout)
+    return fig
+    # print (tls.get_embed(py.plot(fig)))
